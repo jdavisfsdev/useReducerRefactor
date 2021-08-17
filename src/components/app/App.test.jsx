e6ce85b78ffe5d39146f2610e8afaa878ead2ffd
Joseph Davis <joethebartenderpdx@gmail.com>
2021-08-16 18:02:53 -0700

    behavior tests

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
-  afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+  it('Behavior testing', () => {
+    render(<App />);
+
+    const display = screen.getByTestId('display');
+    const redo = screen.getByTestId('redo');
+    const undo = screen.getByTestId('undo');
+    const input = screen.getByTestId('input');
+
+    fireEvent.change(input, { target: { value: '#00FF00' } });
+    console.log(input.value);
+    expect(input.value).toBe('#00ff00');
+
+    fireEvent.click(undo);
+    expect(input.value).toBe('#ff0000');
+
+    fireEvent.click(redo);
+    expect(input.value).toBe('#00ff00');
+
   });
 });
